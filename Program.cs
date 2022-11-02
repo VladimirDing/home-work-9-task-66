bf179b7301e4197f294dfea607bdad5eacb40d9b
Bob Ding <budda72@mail.ru>
2022-11-02 19:21:32 +0600

    task 2 home work 9

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задайте значения M и N. Напишите программу, которая найдёт 
+   сумму натуральных элементов в промежутке от M до N.
+
+   M = 1; N = 15 -> 120
+   M = 4; N = 8. -> 30
+*/
+Console.Clear();
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+// Метод подсчета всех элементов в промежутке от M до N
+int SumNaturalElements(int m, int n)
+{
+    int sum = 0;
+    sum = ((m + n) * (n - m + 1)) / 2;
+    return sum;
+}
+
+int m = GetNumber("Введите число m:");
+int n = GetNumber("Введите число n:");
+Console.WriteLine("Сумма чисел в промежутке от m до n: " + SumNaturalElements(m, n));
